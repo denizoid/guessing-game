2e66949c1ab2c8dd0682f945f52dd7eb3866b2ff
Denis Ahinski <ahinski.denis@gmail.com>
2019-10-08 20:50:57 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.list = [];
+    }
 
     setRange(min, max) {
-
+        for (min; min <= max; min++) {
+            this.list.push(min);
+        }
     }
 
     guess() {
-
+        return this.list[Math.floor(this.list.length/2)];
     }
 
     lower() {
-
+        this.list = this.list.slice(0, Math.floor(this.list.length/2 + 1));
     }
 
     greater() {
-
+        this.list = this.list.slice(Math.floor(this.list.length/2));
     }
 }
 
